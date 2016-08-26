202b7622109f0e0b137c4181c53ad7c248e55dc8
praiseLod <15960123876@163.com>
2016-08-26 14:27:44 +0800

    阶段1

--- doc/mgr_menu.sql
+++ doc/mgr_menu.sql
@@ -1,5 +1,5 @@
 ﻿# Host: localhost  (Version: 5.5.19)
-# Date: 2016-08-26 11:55:10
+# Date: 2016-08-26 14:26:43
 # Generator: MySQL-Front 5.3  (Build 4.205)
 
 /*!40101 SET NAMES utf8 */;
@@ -21,7 +21,7 @@ CREATE TABLE `mgr_menu` (
   `mu_Descr` varchar(200) DEFAULT '' COMMENT '解释',
   `mu_Glyph` int(11) DEFAULT '0' COMMENT 'glyph表示的图标',
   `mu_Expand` int(11) NOT NULL DEFAULT '0' COMMENT '是否展开',
-  `mu_Usability` varchar(64) NOT NULL DEFAULT 'VISIBLE' COMMENT '可用性',
+  `dic_Usability` varchar(64) NOT NULL DEFAULT 'VISIBLE' COMMENT '可用性',
   PRIMARY KEY (`mu_Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='菜单';
 
@@ -29,4 +29,4 @@ CREATE TABLE `mgr_menu` (
 # Data for table "mgr_menu"
 #
 
-INSERT INTO `mgr_menu` VALUES ('A001','博客管理','','fa-tencent-weibo','',1,'','NAVIGATOR','博客前台管理',61909,0,'ENABLE'),('A0010001','博客用户','A001','fa-users','',1,'','NAVIGATOR','博客注册用户管理',61632,0,'ENABLE'),('A0010002','文章管理','A001','fa-wpforms','',2,'','NAVIGATOR','博客用户文章管理',62104,0,'ENABLE'),('B001','统计分析','','fa-area-chart ','',2,'','NAVIGATOR','博客统计分析',61950,0,'ENABLE'),('B0010001','用户统计','B001','fa-bar-chart','',1,'','NAVIGATOR','',61568,0,'ENABLE'),('C001','系统管理','','fa-cogs','',8,'','NAVIGATOR','系统管理',61573,1,'ENABLE'),('C0010001','数据字典','C001','fa-university ','',1,'','NAVIGATOR','数据字典',61852,0,'ENABLE'),('C0010003','系统用户','C001','fa-users',' ',3,'BlogMgr.view.user.UserHome','NAVIGATOR','系统后台用户管理',61632,0,'ENABLE'),('C0010005','权限配置','C001','fa-user-secret','',5,'','NAVIGATOR','后台系统权限配置',61979,0,'ENABLE'),('C0010006','菜单配置','C001','fa-bars','',6,'BlogMgr.view.menu.MenuHome','NAVIGATOR','系统菜单管理',61641,0,'ENABLE'),('C0010007','登录日志','C001','fa-shield','',7,'','NAVIGATOR','前后台登录日志',61746,0,'ENABLE'),('C0010010','系统配置','C001','fa-cog','',10,'','NAVIGATOR','后台系统参数配置',61459,0,'ENABLE'),('D001','个人中心','','fa-user','',9,'','NAVIGATOR','当前用户管理',61447,0,'ENABLE'),('D0010001','本人信息','D001','fa-info','',1,'','NAVIGATOR','当前用户',61737,0,'ENABLE'),('Z001','开发者中心','','fa-bug','',10,'','NAVIGATOR','开发者调式工具',61832,0,'ENABLE'),('Z0010008','异常日志','Z001','fa-exclamation-triangle','',8,'','NAVIGATOR','前后台异常日志',61553,0,'ENABLE'),('Z0010009','jdbc日志','Z001','fa-database','',9,'','NAVIGATOR','数据库操作日志',61888,0,'ENABLE');
+INSERT INTO `mgr_menu` VALUES ('A001','博客管理','','','',1,'','NAVIGATOR','博客前台管理',58917,0,'ENABLE'),('A0010001','博客用户','A001','','',1,'','NAVIGATOR','博客注册用户管理',58901,0,'ENABLE'),('A0010002','文章管理','A001','','',2,'','NAVIGATOR','博客用户文章管理',59000,0,'ENABLE'),('B001','统计分析','','','',2,'','NAVIGATOR','博客统计分析',59012,0,'ENABLE'),('B0010001','用户统计','B001','','',1,'','NAVIGATOR','',59012,0,'ENABLE'),('C001','系统管理','','','',8,'','NAVIGATOR','系统管理',59019,1,'ENABLE'),('C0010001','数据字典','C001','','',1,'','NAVIGATOR','数据字典',58912,0,'ENABLE'),('C0010003','系统用户','C001','',' ',3,'BlogMgr.view.user.UserHome','NAVIGATOR','系统后台用户管理',58905,0,'ENABLE'),('C0010005','权限配置','C001','','',5,'','NAVIGATOR','后台系统权限配置',59060,0,'ENABLE'),('C0010006','菜单配置','C001','','',6,'BlogMgr.view.menu.MenuHome','NAVIGATOR','系统菜单管理',58974,0,'ENABLE'),('C0010007','登录日志','C001','','',7,'','NAVIGATOR','前后台登录日志',59002,0,'ENABLE'),('C0010010','系统配置','C001','','',10,'','NAVIGATOR','后台系统参数配置',58977,0,'ENABLE'),('D001','个人中心','','','',9,'','NAVIGATOR','当前用户管理',58902,0,'ENABLE'),('D0010001','本人信息','D001','','',1,'','NAVIGATOR','当前用户',58919,0,'ENABLE'),('Z001','开发者中心','','','',10,'','NAVIGATOR','开发者调式工具',58974,0,'ENABLE'),('Z0010008','异常日志','Z001','','',8,'','NAVIGATOR','前后台异常日志',58926,0,'ENABLE'),('Z0010009','jdbc日志','Z001','','',9,'','NAVIGATOR','数据库操作日志',58881,0,'ENABLE');
