942ec5e73a6aae9e04edfc663a50c3d04b1bc5cf
PriseLord <15960123876@163.com>
2017-01-12 22:16:35 +0800

    权限设计

--- doc/sys_dictype.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `sys_dictype` (`dt_Id`, `dt_name`, `dt_descr`, `dt_status`) VALUES ('Common', '通用类型', '', 0);
-INSERT INTO `sys_dictype` (`dt_Id`, `dt_name`, `dt_descr`, `dt_status`) VALUES ('Gender', '性别', '', 0);
-INSERT INTO `sys_dictype` (`dt_Id`, `dt_name`, `dt_descr`, `dt_status`) VALUES ('MenuType', '菜单类型', '', 0);
-INSERT INTO `sys_dictype` (`dt_Id`, `dt_name`, `dt_descr`, `dt_status`) VALUES ('UserStatus', '用户状态', '', 0);
